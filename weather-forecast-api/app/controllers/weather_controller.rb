0658df0e5922906f1f11aee15f913fd156789c20
Andres Viviani <andresviviani1@gmail.com>
2023-10-11 22:17:43 -0400

    chore(weather): add error handling

--- weather-forecast-api/app/controllers/weather_controller.rb
+++ weather-forecast-api/app/controllers/weather_controller.rb
@@ -5,5 +5,13 @@ class WeatherController < ApplicationController
     response = Weather::ByZipCodeService.execute(params[:zip_code])
     presented_weather = Weather::ByZipCodePresenter.new(response)
     render jsonapi: presented_weather, class: { 'Weather::ByZipCodePresenter': Weather::SerializableByZipCode }
+  rescue RestClient::NotFound
+    render jsonapi: nil, status: :not_found
+  rescue RestClient::Unauthorized
+    render jsonapi: nil, status: :unauthorized
+  rescue RestClient::Forbidden
+    render jsonapi: nil, status: :forbidden
+  rescue StandardError
+    render jsonapi: nil, status: :internal_server_error
   end
 end
